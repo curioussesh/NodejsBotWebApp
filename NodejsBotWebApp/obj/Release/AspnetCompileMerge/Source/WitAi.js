d0b141e799f03c1330aa7d09fcd7864980b25153
curioussesh <seshadri.skv@gmail.com>
2016-08-08 07:06:31 -0400

    changes1

---
+++ NodejsBotWebApp/obj/Release/AspnetCompileMerge/Source/WitAi.js
@@ -0,0 +1,25 @@
+﻿var request = require('request');
+
+
+var headers = {
+    'Authorization': 'Bearer KMQ36DTAU7N6EP4JDFIBX7IZLLKYKZ5T'
+};
+
+var options = {
+    url: "https://api.wit.ai/message?v=20160526&q=hello",
+    headers: headers
+};
+
+function callback(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        console.log(response);
+    }
+}
+
+function requestwitai(querymsg) {
+    //console.log("hi");
+    //console.log(querymsg);
+    alert('hi1');
+    return "reply";
+    //request(options, callback);
+}
---
+++ NodejsBotWebApp/obj/Release/Package/PackageTmp/chat.js
@@ -0,0 +1,34 @@
+﻿'use strict';
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//let Wit = null;
+//try {
+//    // if running from repo
+//    Wit = require('../').Wit;
+//} catch (e) {
+//    Wit = require('node-wit').Wit;
+//}
+
+
+const accessToken = 'KMQ36DTAU7N6EP4JDFIBX7IZLLKYKZ5T';
+
+
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/chat.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(1337, function () {
+    console.log('listening on *:1337');
+});
+
+
